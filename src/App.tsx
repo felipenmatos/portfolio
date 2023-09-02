ca3a6fd5293f1e8fbf10baf3f37034e81b6ccc84
Felipe Nonato <notfelipe2@hotmail.com>
2023-09-02 00:50:31 -0300

    feat: import of components to be re-rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 
 import './App.css'
 import { About } from './components/About'
+import { Hero } from './components/Hero'
+import { Project } from './components/Project'
+import { Service } from './components/Services'
+import { Skills } from './components/Skills'
+import { Testmonials } from './components/Testmonials'
 
 
 function App() {
 
   return (
     <>
+      <Hero />
       <About />
+      <Service />
+      <Project />
+      <Skills />
+      <Testmonials />
     </>
   )
 }
